750e75dd18ef23ae26939c67d7e40a1f7e119868
Esteban Varas <esteban@slashstudio.cl>
2023-10-26 09:34:27 -0600

    firebase update

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,14 +1,15 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfigProd = {
-  apiKey: 'AIzaSyD_eQdXtVHlNlHbakeFNlVD9AtoQOVrmec',
-  authDomain: 'mountainpass-a0be3.firebaseapp.com',
-  projectId: 'mountainpass-a0be3',
-  storageBucket: 'mountainpass-a0be3.appspot.com',
-  messagingSenderId: '780750827578',
-  appId: '1:780750827578:web:49cfab0985b22899164503',
+  apiKey: "AIzaSyBNFjlWEU2zeTwvzpyNlew21kXQCOl836A",
+  authDomain: "front-vercel.firebaseapp.com",
+  projectId: "front-vercel",
+  storageBucket: "front-vercel.appspot.com",
+  messagingSenderId: "925268636766",
+  appId: "1:925268636766:web:172423a6dbf85bd22870cf",
+  measurementId: "G-C51DPMLW8X",
 };
 
 if (!firebase.apps.length) {
