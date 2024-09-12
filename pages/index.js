b2c95a3b63afcf8b931b82ba0c360675d9130cac
Smarter ChatBOT <smarterbotcl@gmail.com>
2024-09-12 15:24:32 -0300

    V1524

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
                   Automatiza tus procesos con nuestras soluciones avanzadas.
                   <b> Optimiza tu empresa con Make</b> para automatizar tareas y personaliza chatbots entrenados con la información de tu empresa para mejorar la atención al cliente y aumentar las ventas.
                 </p>
-                ¡Prueba gratuita de 15 días y usa los cupones de descuento disponibles!
+                ¡Prueba gratuita de 30 días con Shopify, Chatbot de WhatsApp y Make!
                 <a
                   className="btn btn-primary btn-lg mt-4 mb-4"
                   href="/subscribe"
@@ -76,6 +76,7 @@ export default function Home() {
             </div>
           </div>
         </div>
+
         <div className="section-shop" id="suscribete">
           <div className="container">
             <h1 className="display-1 text-center mb-4">
@@ -83,94 +84,45 @@ export default function Home() {
             </h1>
             <div className="row justify-content-center">
               <PlanCard
-                title="Básico"
-                text1="Gratis"
-                text2="Sí"
-                text3="No"
-                text4="Uno a elección"
-                text5="24 horas"
-                text6="No"
+                title="Prueba o Freemium"
+                text1="Gratis (30 días)"
+                text2="Shopify + WhatsApp + Make"
+                text3="Automatización limitada"
+                text4="1 Integración (Make)"
+                text5="Optimización básica"
+                text6="Sí"
                 modal={false}
                 link="/subscribe"
                 linkText="Regístrate Gratis"
               />
               <PlanCard
-                title="Pro"
-                text1="Precios por categorías"
-                text2="Sí"
-                text3="Sí"
-                text4="Todos"
-                text5="30 días"
-                text6="Sí"
+                title="Premium"
+                text1="Desde $XX/mes"
+                text2="Integraciones múltiples"
+                text3="Licencia ChatGPT, Claude, y Make"
+                text4="META API para chatbot"
+                text5="Automatización al 50% con AI"
+                text6="40 horas de soporte mensual"
                 modal={true}
                 link="/subscribe/pro"
                 linkText="Comprar Suscripción"
               />
+              <PlanCard
+                title="Custom"
+                text1="A consultar"
+                text2="Solo para Shopify ($2300+)"
+                text3="Licencias Gemini y Make"
+                text4="Integración con BSALE, DEFONTANA, ENVIAME.IO"
+                text5="40 horas semanales de soporte"
+                text6="Setup en Google Workspace"
+                modal={true}
+                link="/subscribe/custom"
+                linkText="Consultar"
+              />
             </div>
           </div>
         </div>
-        <div className="section-white">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-6 mx-auto text-center">
-                <img
-                  className="align-middle img-fluid rounded img-home-two"
-                  src="../images/Meet_Pedro_Zaffuto.jpeg"
-                  width="460"
-                  alt="Beneficios"
-                />
-              </div>
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-6 z-index">
-                <h1>
-                  Cupones <span className="color">de Descuento y Beneficios</span>
-                </h1>
-                <p className="large mt-3">
-                  Tus suscriptores disfrutarán de descuentos en servicios adicionales como alojamiento, renta temporal, clases y mucho más.
-                </p>
 
-                <div>
-                  <br />
-                  <a
-                    className="btn btn-primary btn-md mt-4"
-                    href="/preguntas-frecuentes"
-                  >
-                    ¿Cómo funciona?
-                  </a>
-                  <a
-                    className="btn btn-primary btn-md mt-4 mx-3"
-                    href="/precios"
-                  >
-                    Tarifas
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="section-white mb-2">
-          <div className="container">
-            <div className="row align-content-start align-items-center">
-              <div className="col-12 col-sm-12 col-md-8 mx-auto text-center">
-                <h1 className="mb-5">
-                  Conoce <span className="color">Smarterbot</span>
-                </h1>
-                <div className="justify-content-center">
-                  <div className="video-home rounded">
-                    <div className="embed-container">
-                      <iframe
-                        src="https://www.youtube.com/embed/Mnfzklo2CVw?start=0"
-                        frameBorder="0"
-                        allowFullScreen
-                        title="Video de Smarterbot"
-                      ></iframe>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <Footer />
       </div>
     </Fragment>
