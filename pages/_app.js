8add32b05dea985f40640e542b05dad5ba5da330
Smarter ChatBOT <smarterbotcl@gmail.com>
2024-09-16 09:56:43 -0300

    V0956

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  // Este hook asegura que Bootstrap solo se cargue en el cliente (navegador)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Cargar el script de Bootstrap solo en el cliente
+      require('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
+  }, []); // El array vacío hace que se ejecute solo una vez, después de que el componente se monte
+
   return <Component {...pageProps} />;
 }
