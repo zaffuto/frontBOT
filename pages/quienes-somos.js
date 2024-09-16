978fe1bd834f891d216b8709e2ff3b3d7646ebe0
Smarter ChatBOT <smarterbotcl@gmail.com>
2024-09-16 00:13:30 -0300

    V00013

--- pages/quienes-somos.js
+++ pages/quienes-somos.js
@@ -1,5 +1,5 @@
 import { Montserrat } from "@next/font/google";
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
@@ -7,53 +7,71 @@ import Footer from "../components/Footer";
 const montserrat = Montserrat({ subsets: ["latin"], weight: "variable" });
 
 export default function QuienesSomos() {
+  const teamMembers = [
+    {
+      name: "Pedro José Zaffuto",
+      role: "Especialista en IA y SCM",
+      image: "https://placehold.co/525x687",
+      description:
+        "Pedro es un experto en la adopción de Inteligencia Artificial en sistemas SCM, con más de 10 años de experiencia en el sector.",
+    },
+    {
+      name: "María López",
+      role: "Gerente de Operaciones",
+      image: "https://placehold.co/525x687",
+      description:
+        "María lidera nuestro equipo de operaciones, asegurando que todos los proyectos se ejecuten con la máxima eficiencia.",
+    },
+    {
+      name: "Juan Pérez",
+      role: "Desarrollador Senior",
+      image: "https://placehold.co/525x687",
+      description:
+        "Juan es responsable del desarrollo de nuestras soluciones tecnológicas innovadoras.",
+    },
+  ];
+
   return (
     <Fragment>
-      <Header title="Quienes Somos"></Header>
+      <Header title="Quiénes Somos" />
       <div className={`${montserrat.className} d-flex flex-column h-100`}>
         <Nav />
         <div className="section-single">
           <div className="container">
-            <div className="row">
+            <div className="row text-center">
               <div className="col-12">
-                <h4 className="text-center text-uppercase">¿Quiénes somos?</h4>
-                <h1 className="mb-5 text-center">
+                <h4 className="text-uppercase">¿Quiénes somos?</h4>
+                <h1 className="mb-5">
                   <span className="color">Smarter</span> Bot
                 </h1>
               </div>
             </div>
-            <div className="row align-content-start align-items-center">
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-6 mx-auto text-center">
-                <img
-                  className="align-middle img-fluid rounded img-home-two"
-                  src="https://placehold.co/525x687"
-                  width="460"
-                />
-              </div>
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-6 z-index">
-                <p className="large mt-3">
-                  <strong>Lorem Ipsum</strong> es simplemente el texto de
-                  relleno de las imprentas y archivos de texto.
-                </p>
-                <p>
-                  {" "}
-                  Lorem Ipsum ha sido el texto de relleno estándar de las
-                  industrias desde el año 1500, cuando un impresor (N. del T.
-                  persona que se dedica a la imprenta) desconocido usó una
-                  galería de textos y los mezcló de tal manera que logró hacer
-                  un libro de textos especimen. No sólo sobrevivió 500 años,
-                  sino que tambien ingresó como texto de relleno en documentos
-                  electrónicos, quedando esencialmente igual al original. Fue
-                  popularizado en los 60s con la creación de las hojas
-                  "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
-                  recientemente con software de autoedición, como por ejemplo
-                  Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
-                </p>
-              </div>
+            <div className="row">
+              {teamMembers.map((member, index) => (
+                <div
+                  className="col-12 col-sm-12 col-md-6 col-xl-4 mb-4"
+                  key={index}
+                >
+                  <div className="card h-100 text-center">
+                    <img
+                      className="card-img-top img-fluid rounded"
+                      src={member.image}
+                      alt={member.name}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{member.name}</h5>
+                      <p className="card-text">
+                        <strong>{member.role}</strong>
+                      </p>
+                      <p className="card-text">{member.description}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-        <Footer></Footer>
+        <Footer />
       </div>
     </Fragment>
   );
