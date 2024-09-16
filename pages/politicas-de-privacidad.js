55463c2030699ff4200e463c0361f1d4f1cccc8a
Smarter ChatBOT <smarterbotcl@gmail.com>
2024-09-16 09:06:56 -0300

    V0906

--- pages/politicas-de-privacidad.js
+++ pages/politicas-de-privacidad.js
@@ -1,241 +1,30 @@
-import { Montserrat } from "next/font/google";
-import { Fragment, useEffect, useState } from "react";
-import { db } from "../services/firebaseService";
+// preguntas-frecuentes.js
+import { Montserrat } from 'next/font/google';
+import { Fragment } from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import PlanCard from "../components/planCard";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: "variable" });
 
-export default function Home() {
-  const [count, setCount] = useState(1);
-  const [price, setPrice] = useState(0);
-  const [offerPrice, setOfferPrice] = useState(0);
-  const [displayMobileBar, setDisplayMoblieBar] = useState(false);
-  const [discountText, setDiscountText] = useState("");
-
-  const updateCount = (option) => {
-    if (option == "minus" && count > 1) {
-      localStorage.setItem("__smtb_count", count - 1);
-      setCount(count - 1);
-    }
-    if (option == "plus" && count < 10) {
-      localStorage.setItem("__smtb_count", count + 1);
-      setCount(count + 1);
-    }
-  };
-
-  useEffect(() => {
-    if (typeof window != "undefined") {
-      localStorage.setItem("__smtb_count", 1);
-    }
-    db.collection("settings")
-      .doc("--")
-      .get()
-      .then((docRef) => {
-        setPrice(parseInt(docRef.data().price));
-        setOfferPrice(parseInt(docRef.data().offerPrice));
-        setDiscountText(docRef.data().discountText);
-      });
-  }, []);
-
+export default function PreguntasFrecuentes() {
   return (
     <Fragment>
-      <Header title="Inicio"></Header>
+      <Header title="Preguntas Frecuentes" />
       <div className={`${montserrat.className} d-flex flex-column h-100`}>
-        <Nav></Nav>
-        <div className="section-cover">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-7 z-index">
-                <h1 className="display-1">
-                  Fullday
-                  <span className="color">GO</span>
-                </h1>
-                <p className="large mt-3">
-                  Descripción de los servicios de FulldayGO{" "}
-                  <b>importante completar</b>
-                </p>
-                <p className="large mt-3">
-                  ¡Inscríbete gratis en nuestros planes y conoce los cupones de descuento de
-                  la tienda!
-                </p>
-                <a
-                  className="btn btn-primary btn-lg mt-4 mb-4"
-                  href="/subscribe"
-                >
-                  Inscríbete gratis
-                </a>
-              </div>
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-5 mx-auto text-center img-home-container">
-                <img
-                  className="align-middle img-fluid rounded img-home"
-                  src="https://placehold.co/558x730"
-                  width="460"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="section-shop" id="suscribete">
-          <div className="container">
-            <h1 className="display-1 text-center mb-4">
-              Elige tu <span className="color">Plan</span>
-            </h1>
-            <div className="row justify-content-center">
-              <PlanCard
-                title="Basic"
-                text1="Gratis"
-                text2="Sí"
-                text3="No"
-                text4="Uno a elección"
-                text5="78 horas"
-                text6="No"
-                modal={false}
-                link="/subscribe"
-                linkText="Inscríbete Gratis"
-              ></PlanCard>
-              <PlanCard
-                title="Pro"
-                text1="Valores por categorías"
-                text2="Sí"
-                text3="Sí"
-                text4="Todos"
-                text5="24 horas"
-                text6="Sí"
-                modal={true}
-                link="/subscribe/pro"
-                linkText="Comprar Suscripción"
-              ></PlanCard>
-            </div>
-          </div>
-        </div>
-        <div className="section-white">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-6 mx-auto text-center">
-                <img
-                  className="align-middle img-fluid rounded img-home-two"
-                  src="https://placehold.co/558x730"
-                  width="460"
-                />
-              </div>
-              <div className="col-12 col-sm-12 col-lg-12 col-xl-6 z-index">
-                <h1>
-                  Una montaña <span className="color">de beneficios</span>
-                </h1>
-                <p className="large mt-3">
-                  Los suscriptores de FulldayGO podrán disfrutar de tarifas de
-                  ski por hora, ofertas especiales en alojamiento, rental,
-                  clases, alimentación y mucho más.
-                </p>
-
-                <div>
-                  <br />
-                  <a
-                    className="btn btn-primary btn-md mt-4"
-                    href="/preguntas-frecuentes"
-                  >
-                    ¿Cómo funciona?
-                  </a>
-                  <a
-                    className="btn btn-primary btn-md mt-4 mx-3"
-                    href="/precios"
-                  >
-                    Tarifas
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="section-white mb-2">
+        <Nav />
+        <div className="section-single">
           <div className="container">
             <div className="row align-content-start align-items-center">
-              <div className="col-12 col-sm-12 col-md-8 mx-auto text-center">
-                <h1 className="mb-5">
-                  Conoce <span className="color">FulldayGO</span>
+              <div className="col-12 col-sm-12 col-md-10 mx-auto">
+                <h1 className="mb-5 text-center">
+                  <span className="color">Preguntas</span> Frecuentes
                 </h1>
-                <div className="justify-content-center">
-                  <div className="video-home rounded">
-                    <div className="embed-container">
-                      <iframe
-                        src="https://www.youtube.com/embed//njrhms-83qc"
-                        frameBorder="0"
-                        allowFullScreen=""
-                      ></iframe>
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
+            {/* Resto del contenido */}
           </div>
         </div>
-        {/*<div className="section-white mb-5">
-          <div className="container">
-            <div className="row align-content-start align-items-center">
-              <div className="col-12 col-sm-12 col-md-12 mx-auto text-center">
-                <h1 className="mb-5">Centros de ski asociados</h1>
-                <div className="partner-container d-flex justify-content-center">
-                  <div className="partner-brand">
-                    <a
-                      href="https://laparva.cl/"
-                      target="_blank"
-                      title="La Parva"
-                    >
-                      <img
-                        className="img-fluid"
-                        src="/images/la-parva.png"
-                        width="180"
-                      />
-                    </a>
-                  </div>
-                  <div className="partner-brand">
-                    <a
-                      href="https://skiportillo.com/"
-                      target="_blank"
-                      title="Ski Portillo"
-                    >
-                      <img
-                        className="img-fluid"
-                        src="/images/ski-portillo.png"
-                        width="180"
-                      />
-                    </a>
-                  </div>
-                  <div className="partner-brand">
-                    <a
-                      href="https://antillanca.cl/"
-                      target="_blank"
-                      title="Antillanca - Vive la Montaña"
-                    >
-                      <img
-                        className="img-fluid"
-                        src="/images/antillanca.png"
-                        width="180"
-                      />
-                    </a>
-                  </div>
-                  <div className="partner-brand">
-                    <a
-                      href="https://laderas.com.ar/"
-                      target="_blank"
-                      title="Laderas – Cerro Perito Moreno"
-                    >
-                      <img
-                        className="img-fluid"
-                        src="/images/laderas.png"
-                        width="180"
-                      />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          </div>*/}
-        <Footer></Footer>
       </div>
     </Fragment>
   );
--- pages/preguntas-frecuentes.js
+++ pages/preguntas-frecuentes.js
@@ -1,6 +1,4 @@
-// preguntas-frecuentes.js
 import { Montserrat } from '@next/font/google';
-import { Fragment } from "react";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
@@ -9,7 +7,7 @@ const montserrat = Montserrat({ subsets: ["latin"], weight: "variable" });
 
 export default function PreguntasFrecuentes() {
   return (
-    <Fragment>
+    <>
       <Header title="Preguntas Frecuentes" />
       <div className={`${montserrat.className} d-flex flex-column h-100`}>
         <Nav />
@@ -101,182 +99,7 @@ export default function PreguntasFrecuentes() {
                         </div>
                       </div>
                     </div>
-                    {/* Pregunta 4 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingFour">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseFour"
-                          aria-expanded="false"
-                          aria-controls="collapseFour"
-                        >
-                          ¿Qué tipo de soporte ofrecen después de la implementación?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseFour"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingFour"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Ofrecemos soporte continuo ajustado al plan de mantenimiento para asegurar que el chatbot funcione de manera óptima. Nuestro equipo está disponible para resolver cualquier duda o inconveniente que pueda surgir.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Pregunta 5 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingFive">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseFive"
-                          aria-expanded="false"
-                          aria-controls="collapseFive"
-                        >
-                          ¿Cómo garantizan la seguridad y confidencialidad de mi información?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseFive"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingFive"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Firmamos un contrato de confidencialidad que garantiza que la información proporcionada no será compartida ni revelada a terceros. Implementamos medidas de seguridad estrictas para proteger tus datos y cumplir con las regulaciones vigentes en materia de protección de datos.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Pregunta 6 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingSix">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseSix"
-                          aria-expanded="false"
-                          aria-controls="collapseSix"
-                        >
-                          ¿Es necesario realizar reuniones presenciales?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseSix"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingSix"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Sí, creemos en el valor del contacto directo. Realizamos un mínimo de 40 horas en terreno y reuniones presenciales para entender a fondo tu negocio y asegurar una implementación exitosa del chatbot en tu empresa.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Pregunta 7 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingSeven">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseSeven"
-                          aria-expanded="false"
-                          aria-controls="collapseSeven"
-                        >
-                          ¿Qué sucede si necesito personalizaciones específicas?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseSeven"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingSeven"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Nos especializamos en desarrollar soluciones a medida. Trabajaremos contigo para entender tus requerimientos específicos y adaptar el chatbot para satisfacer tus necesidades particulares.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Pregunta 8 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingEight">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseEight"
-                          aria-expanded="false"
-                          aria-controls="collapseEight"
-                        >
-                          ¿Ofrecen garantías sobre sus servicios?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseEight"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingEight"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Sí, ofrecemos un seguro de "libre riesgo" y una garantía de 12 meses sobre nuestros servicios. Nos comprometemos a entregar soluciones de calidad y respaldamos nuestro trabajo para asegurar tu satisfacción.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Pregunta 9 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingNine">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseNine"
-                          aria-expanded="false"
-                          aria-controls="collapseNine"
-                        >
-                          ¿Cómo puedo obtener una cuenta DEMO?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseNine"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingNine"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Puedes contactarnos a través de nuestro formulario en la página de <a href="/contacto">contacto</a>, enviarnos un correo electrónico a <a href="mailto:hola@smarterbot.cl">hola@smarterbot.cl</a> o llamarnos al <a href="tel:+56979540471">+56 9 7954 0471</a>. También puedes agendar una reunión directamente vía WhatsApp haciendo clic <a href="https://api.whatsapp.com/send/?phone=56979540471&text=Hola%2C+quiero+obtener+una+cuenta+demo+de+SmarterBot...&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">aquí</a>. Estaremos encantados de analizar tus necesidades y proporcionarte una cuenta DEMO personalizada.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Pregunta 10 */}
-                    <div className="accordion-item">
-                      <h2 className="accordion-header" id="headingTen">
-                        <button
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          aria-expanded="false"
-                          aria-controls="collapseTen"
-                          data-bs-target="#collapseTen"
-                        >
-                          ¿Qué experiencia tienen en mi industria?
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseTen"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingTen"
-                        data-bs-parent="#faqAccordion"
-                      >
-                        <div className="accordion-body">
-                          Hemos trabajado con empresas de diversos sectores, incluyendo logística, retail, servicios financieros y más. Nuestro enfoque se adapta a las necesidades específicas de cada industria, y contamos con la experiencia de Pedro José Zaffuto en adopción de inteligencia artificial en sistemas SCM.
-                        </div>
-                      </div>
-                    </div>
-                    {/* Puedes agregar más preguntas y respuestas siguiendo el mismo formato */}
+                    {/* Resto de las preguntas */}
                   </div>
                 </div>
               </div>
@@ -284,6 +107,7 @@ export default function PreguntasFrecuentes() {
             <Footer />
           </div>
         </div>
-      </Fragment>
-    );
+      </div>
+    </>
+  );
 }
